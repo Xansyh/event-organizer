75de7d0526499d499c78df720aa6c924e05fd045
Xansyh <xansyh@gmail.com>
2020-07-04 22:59:32 +0800

    app deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCJ6MF9kwupyWjHMoqZLcZTwedmiYwl-dc",
+    authDomain: "eventorganizer-70ba6.firebaseapp.com",
+    databaseURL: "https://eventorganizer-70ba6.firebaseio.com",
+    projectId: "eventorganizer-70ba6",
+    storageBucket: "eventorganizer-70ba6.appspot.com",
+    messagingSenderId: "627559646174",
+    appId: "1:627559646174:web:fdeac46afa6d4ce960534f",
+    measurementId: "G-LVN7WQP2QT"
+  }
 };
